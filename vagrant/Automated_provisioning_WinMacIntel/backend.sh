a217e6041a5fae1e569d4a7ea4f85bc58635d29d
Nas <n.choudhury1@live.co.uk>
2024-06-24 18:31:52 +0100

    vagrant up terminal error, ssh cmd no response

--- vagrant/Automated_provisioning_WinMacIntel/backend.sh
+++ vagrant/Automated_provisioning_WinMacIntel/backend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DATABASE_PASS='admin12345'
+DATABASE_PASS='admin123'
 
 # MEmcache
 yum install epel-release -y
@@ -43,8 +43,8 @@ mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
 mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
 mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
 mysql -u root -p"$DATABASE_PASS" -e "create database accounts"
-mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'localhost' identified by 'admin12345'"
-mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'app01' identified by 'admin12345'"
+mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'localhost' identified by 'admin123'"
+mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'app01' identified by 'admin123'"
 mysql -u root -p"$DATABASE_PASS" accounts < /vagrant/vprofile-repo/src/main/resources/db_backup.sql
 mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
 
